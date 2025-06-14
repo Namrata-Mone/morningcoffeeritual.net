2e9a5d42c467e818bd21045694f5ae23ce8441a8
Namrata Mone <nmrtmn@gmail.com>
2025-06-15 01:28:36 +0400

    first commit

---
+++ external/google-analytics.js
@@ -0,0 +1,10 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+<!-- End Global site tag (gtag.js) - Google Analytics -->
+
+
+<!-- GA4 -->
+gtag('config', 'G-8HT0XPV92P');
